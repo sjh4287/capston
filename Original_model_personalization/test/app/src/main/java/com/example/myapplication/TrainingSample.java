7f53130153392af5c64c5575a3cf0cf3eef4a666
권철현 <kkcchh9999@naver.com>
2022-01-30 21:06:53 +0900

    고쳐냈다!

---
+++ Original_model_personalization/test/app/src/main/java/com/example/myapplication/TrainingSample.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+public class TrainingSample {
+	float[] bottleneck;
+	float[] label;
+
+	TrainingSample(float[] bottleneck, float[] label) {
+		this.bottleneck = bottleneck;
+		this.label = label;
+	}
+}
